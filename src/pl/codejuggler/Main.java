0de45ea5f178b9d69b208a3b1e005ff66865d699
Sebastian <seba@MacBook-Pro-Sebastian.local>
2018-08-06 23:10:58 +0200

    Added README and modified Main.java

--- src/pl/codejuggler/Main.java
+++ src/pl/codejuggler/Main.java
@@ -3,6 +3,6 @@ package pl.codejuggler;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello world");
+	    System.out.println("Hello world! I love you all...:)");
     }
 }
